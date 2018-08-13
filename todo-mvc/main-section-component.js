0214391da5b9ae2e5b80a384fb590386590696f9
Darren <darren@develer.co.uk>
2018-08-13 10:57:01 +0100

    Update main-section-component.js

--- todo-mvc/main-section-component.js
+++ todo-mvc/main-section-component.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Component from "component";
 import TodoItem from "todo-item-component";
-import TodoStore$ from "todo-store";
+import Todo$ from "todo-store";
 
 const MainSection = new Component();
 
-MainSection.addStore(TodoStore$, TodoStore$.getFilter("ARE_ALL_COMPLETE"));
+MainSection.addStore(Todo$, Todo$.getFilter("ARE_ALL_COMPLETE"));
 
 MainSection.on("render", (state, props) => (
   <section id="main">
@@ -16,14 +16,14 @@ MainSection.on("render", (state, props) => (
       checked={state.areAllComplete}
     />
     <label htmlFor="toggle-all">Mark all as complete</label>
-    <ul id="todo-list">{MainSection.triggerFirst("renderTodos")}</ul>
+    <ul id="todo-list">{MainSection.triggerFirst("render_todos")}</ul>
   </section>
 ));
 
-MainSection.on("renderTodos", (state, props) =>
+MainSection.on("render_todos", (state, props) =>
   Object.keys(state).map(key => <TodoItem key={key} id={key} />)
 );
 
 MainSection.on("change", () => {
-  TodoStore$.triggerAction("TOGGLE_COMPLETE_ALL");
+  Todo$.triggerAction("TOGGLE_COMPLETE_ALL");
 });
